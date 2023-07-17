f50c370e21f9c1b3ae78e09a07faf25f055b1f03
Tomer <37521314+tomerssh@users.noreply.github.com>
2023-07-17 12:17:35 +0300

    part 6 refactor

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ import tcod
 
 from engine import Engine
 import entity_factories
-from input_handlers import EventHandler
 from procgen import generate_dungeon
 
 def toggle_fullscreen(context: tcod.context.Context) -> None:
@@ -37,21 +36,20 @@ def main() -> None:
         "src/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    event_handler = EventHandler()
-
     player = copy.deepcopy(entity_factories.player)
 
-    game_map = generate_dungeon(
+    engine = Engine(player=player)
+
+    engine.game_map = generate_dungeon(
         max_rooms=max_rooms,
         room_min_size=room_min_size,
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
         max_monsters_per_room = max_monsters_per_room,
-        player=player
+        engine=engine,
     )
-
-    engine = Engine(event_handler=event_handler, game_map=game_map, player=player)
+    engine.update_fov()
 
     with tcod.context.new_terminal(
         screen_width,
@@ -64,11 +62,9 @@ def main() -> None:
         while True:
             engine.render(console=root_console, context=context)
 
-            events = tcod.event.wait()
+            engine.event_handler.handle_events()
 
             root_console.clear()
-
-            engine.handle_events(events)
 
             # alt+enter to fullscreen
             keyboard_state = tcod.event.get_keyboard_state()
